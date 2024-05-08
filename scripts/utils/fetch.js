e829aeb9e2e967060360efb8dc116fa457934852
Kevin-Jerbic <94851418+Kevin-Jerbic@users.noreply.github.com>
2024-05-08 17:37:32 +0200

    Add fetch.js

---
+++ scripts/utils/fetch.js
@@ -0,0 +1,19 @@
+const url = '../../data/recipes.js';
+
+/**
+ * Fetch recipe data from a specified URL.
+ *
+ * @async
+ * @function fetchData
+ * @returns {Promise<Array>} A promise that resolves to an array of recipe data.
+ * @throws {Error} If there is an issue with the fetch operation.
+ */
+export async function fetchData() {
+    try {
+        const { recipes } = await import(url);
+        return recipes;
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+        throw error;
+    }
+}
